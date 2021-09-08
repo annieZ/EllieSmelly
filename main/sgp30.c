60f47a9a81530e2f0bcc6e71e7809ff2d3c779dc
Annie <annie@snotra.com>
2021-09-08 16:38:06 -0400

    removing build directory from github

---
+++ main/sgp30.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <math.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+
+#include "global.h"
+#include "cta.h"
+
+
+void aws_sgp30_task(void *param) {
+   
+}
